bc1414420a3c4ab93dd5b4ec1f62a6e2a99fdeb9
Yogesh Janrao <138653873+yogeshjanrao@users.noreply.github.com>
2024-12-23 23:32:09 +0530

    remove country filter

--- src/main/java/com/yogeshjanrao/spring/batch/config/CustomerProcessor.java
+++ src/main/java/com/yogeshjanrao/spring/batch/config/CustomerProcessor.java
@@ -7,10 +7,6 @@ public class CustomerProcessor implements ItemProcessor<Customer,Customer> {
 
     @Override
     public Customer process(Customer customer) throws Exception {
-        if(customer.getCountry().equals("United States")) {
-            return customer;
-        }else{
-            return null;
-        }
+        return customer;
     }
 }
